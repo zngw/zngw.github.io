88dc931cad8ee15568b68903fbd4c2cd46203f39
guoke3915 <guoke3915@163.com>
2020-03-21 12:46:56 +0800

    Site updated: 2020-03-21 12:46:41

--- js/src/photo.js
+++ js/src/photo.js
@@ -20,7 +20,7 @@ photo = {
         if (photoArray == null) {
             $.ajax({
                 type:"GET",
-                url:"//guoke3915.coding.net/p/guoke3915/d/photo/git/raw/master/photo1.jsonp",
+                url:"//guoke3915.coding.net/p/guoke3915/d/photo/git/raw/master/photo.jsonp",
                 dataType:"jsonp",
                 jsonp:"callback",
                 jsonpCallback:"callback",
